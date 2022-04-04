36ce4c548f3fb4dbcc8eb3063e5a5755bd254a7b
emeery <gerardoemery@live.com.mx>
2022-04-04 18:46:15 -0500

    widget basics

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    var questions = ['What\'s your favorite color?','What\s your favorite fruit?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App_First'),
         ), // AppBar
-        body: Text('Default Texto'),
+        body: Column(
+          children: [
+            Text('Cuestionario!'),
+            ElevatedButton(
+              child: Text('Respuesta 1'), onPressed: null
+            ),
+            ElevatedButton(
+              child: Text('Respuesta 2'), onPressed: null
+            ),
+            ElevatedButton(
+              child: Text('Respuesta 3'), onPressed: null
+            ),
+          ],
+        ), // Column
       ), // Scaffold
     ); // MaterialApp
   }
