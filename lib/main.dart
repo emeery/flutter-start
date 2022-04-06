ca0aad8b651830584a3974646a3ef7b24fa11eba
Gerardo Bautista <gerardoemery@live.com.mx>
2022-04-05 19:20:35 -0500

    'onpressed

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void processTheQuestion() {
+    print('imprime una respuesta');
+  }
+
   @override
   Widget build(BuildContext context) {
-    
+    const questions = ['Tu comida fav', 'tu color fav'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('App_First'),
+          title: const Text('App_First'),
         ), // AppBar
-        body: Text('Default Texto'),
+        body: Column(
+          children: [
+            Text('responde la pregunta'),
+            RaisedButton(
+              child: Text('Respuesta 1'),
+              onPressed: () {
+                processTheQuestion();
+              },
+            ),
+          ],
+        ),
       ), // Scaffold
     ); // MaterialApp
   }
