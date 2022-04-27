c59282b815f53952ba4e6f95b47c1b782529688e
Gerardo Bautista <gerardoemery@live.com.mx>
2022-04-26 22:45:08 -0500

    quiz constructor & mssge conditional

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'quiz.dart';
-import 'question.dart';
-import 'answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,12 +21,16 @@ class _MyAppState extends State<MyApp> {
     {
       'question': 'Jugador favorito?',
       'answer': ['Cristiano', 'Messi', 'Neymar']
-    }
+    },
+    {
+      'question': 'Platillo favorito?',
+      'answer': ['Tacos', 'Pizza', 'Hamburguesa']
+    },
   ];
 
-
   void _answerQuestion() {
-    if (_questionIndex == _questions.length) {
+    print('Number of elements in list: ${_questions.length}');
+    if (_questionIndex > _questions.length) {
       print('cayo aqi');
     } else {
       print('cayo aca');
@@ -39,45 +41,55 @@ class _MyAppState extends State<MyApp> {
     print(_questionIndex);
   }
 
-  void _testList() {
-    // print("The number is $_questionIndex");
-    print("the test works");
-    // {question: Color favorito?, answers: [Negro, Azul, Verde]}
-  }
+  // void _testList() {
+  // print("The number is $_questionIndex");
+  // print("the test works");
+  // {question: Color favorito?, answers: [Negro, Azul, Verde]}
+  // }
 
   Widget build(BuildContext context) {
     return MaterialApp(
-
-        // theme: ThemeData(appBarTheme: AppBarTheme(color: const Color(283747),),
-        theme: ThemeData(
-          scaffoldBackgroundColor: Color(0xff123456),
-          colorScheme: ColorScheme(
-            brightness: Brightness.light,
-            primary: Color.fromARGB(255, 49, 67, 90),
-            onPrimary: Colors.white,
-            // Colors that are not relevant to AppBar in LIGHT mode:
-            secondary: Colors.grey,
-            onSecondary: Colors.grey,
-            background: Colors.grey,
-            onBackground: Colors.grey,
-            surface: Colors.grey,
-            onSurface: Colors.grey,
-            error: Colors.grey,
-            onError: Colors.grey,
-          ),
+      // theme: ThemeData(appBarTheme: AppBarTheme(color: const Color(283747),),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff123456),
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: Color.fromARGB(255, 49, 67, 90),
+          onPrimary: Colors.white,
+          // Colors that are not relevant to AppBar in LIGHT mode:
+          secondary: Colors.grey,
+          onSecondary: Colors.grey,
+          background: Colors.grey,
+          onBackground: Colors.grey,
+          surface: Colors.grey,
+          onSurface: Colors.grey,
+          error: Colors.grey,
+          onError: Colors.grey,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'CUESTIONES APP',
-              style: TextStyle(
-                fontFamily: 'Akshar',
-                color: Colors.teal,
-                fontWeight: FontWeight.w500,
-              ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'CUESTIONES APP',
+            style: TextStyle(
+              fontFamily: 'Akshar',
+              color: Colors.teal,
+              fontWeight: FontWeight.w500,
             ),
           ),
-          body: _questionIndex < _questions.length ? Quiz(_answerQuestion _questions) : Center(child: Text('Perfecto!'))
-        ));
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                questionIndex: _questionIndex,
+                answerQuestion: _answerQuestion)
+            : Center(
+                child: Text(
+                  'Gracias por tus respuestas!',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+      ),
+    );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
+import 'answer.dart';
 
 class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final VoidCallback answerQuestion;
+
+  Quiz(
+      {required this.questions,
+      required this.questionIndex,
+      required this.answerQuestion});
   @override
   Widget build(BuildContext context) {
     return Column(
-            children: [
-              Question(
-                questions[_questionIndex]['question'] as String,
-              ),
-              ...(questions[_questionIndex]['answer'] as List<String>)
-                  .map((ans) {
-                return Answer(_answerQuestion, ans);
-              }).toList(),
-              SizedBox(height: 5),
-            ],
-          )
+      children: [
+        Question(
+          questions[questionIndex]['question'] as String,
+        ),
+        ...(questions[questionIndex]['answer'] as List<String>).map((ans) {
+          return Answer(answerQuestion, ans);
+        }).toList(),
+        SizedBox(height: 5),
+      ],
+    );
   }
 }
